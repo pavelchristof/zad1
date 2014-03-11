0907ae6cefd7fb528ee6e02a8ea623baf46e7c52
Paweł Nowak <pawel834@gmail.com>
2014-03-11 11:33:32 +0100

    Added override modifiers.

--- main.cpp
+++ main.cpp
@@ -11,56 +11,56 @@ class Driver : public ParserEventHandler
 {
 public:
 	Driver() : parser(this), statements(0) {}
-	
+
 	void run()
 	{
 		parser.parse(std::cin);
 	}
-	
-	virtual void set(uint64_t key, uint64_t value)
+
+	void set(uint64_t key, uint64_t value) override
 	{
 		if (key > maxKey || value > maxValue || !stmtCheck()) {
 			error();
 			return;
 		}
-		
+
 		uint32_t nodes = bst.set(key, value);
 		std::cout << "wezlow: " << nodes << '\n';
 	}
-	
-	virtual void sum(uint64_t time, uint64_t left, uint64_t right)
+
+	void sum(uint64_t time, uint64_t left, uint64_t right) override
 	{
 		if (time > bst.now() || left > right || right > maxKey || !stmtCheck()) {
 			error();
 			return;
 		}
-		
+
 		uint64_t sum = bst.sum(time, left, right);
 		std::cout << "suma(" << time << ',' << left << ".." << right << ")=" << sum << '\n';
 	}
-	
-	virtual void clear(uint64_t time)
+
+	void clear(uint64_t time) override
 	{
 		if (time > bst.now() || !stmtCheck()) {
 			error();
 			return;
 		}
-		
+
 		uint32_t nodes = bst.clear(time);
 		std::cout << "wezlow: " << nodes << '\n';
 	}
-	
-	virtual void error()
+
+	void error() override
 	{
 		std::cout << "zignorowano\n";
 	}
-	
+
 private:
-	bool stmtCheck() 
+	bool stmtCheck()
 	{
 		return statements++ < maxStatements;
 	}
-	
+
 	BST bst;
 	Parser parser;
 	uint32_t statements;
