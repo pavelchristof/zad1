77ce6d25d0468a349f9593089eec076c2809a92e
Paweł Nowak <pawel834@gmail.com>
2014-03-12 23:03:18 +0100

    I like d pointers.

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,58 +1,80 @@
 #include "driver.hpp"
+#include "parser.hpp"
+#include "bst.hpp"
 
-const uint64_t maxKey = 1000 * 1000 * 1000;
-const uint64_t maxValue = 1000;
-const uint32_t maxStatements = 1000 * 1000;
+struct DriverPrivate : public ParserEventHandler
+{
+	const uint64_t maxKey = 1000 * 1000 * 1000;
+	const uint64_t maxValue = 1000;
+	const uint32_t maxStatements = 1000 * 1000;
+
+	DriverPrivate(std::istream &in, std::ostream &out) :
+		in(in), out(out), parser(this), statements(0)
+	{
+	}
+
+	void assignment(uint64_t key, uint64_t value) override
+	{
+		if (key > maxKey || value > maxValue || !stmtCheck()) {
+			error();
+			return;
+		}
+
+		uint32_t nodes = bst.assignment(key, value);
+		out << "wezlow: " << nodes << '\n';
+	}
+
+	void sum(uint64_t time, uint64_t left, uint64_t right) override
+	{
+		if (time > bst.now() || left > right || right > maxKey || !stmtCheck()) {
+			error();
+			return;
+		}
+
+		uint64_t sum = bst.sum(time, left, right);
+		out << "suma(" << time << ',' << left << ".." << right << ")=" << sum << '\n';
+	}
+
+	void clear(uint64_t time) override
+	{
+		if (time > bst.now() || !stmtCheck()) {
+			error();
+			return;
+		}
+
+		uint32_t nodes = bst.clear(time);
+		out << "wezlow: " << nodes << '\n';
+	}
+
+	void error() override
+	{
+		out << "zignorowano\n";
+	}
+
+	bool stmtCheck()
+	{
+		return statements++ < maxStatements;
+	}
+
+	std::istream &in;
+	std::ostream &out;
+
+	BST bst;
+	Parser parser;
+	uint32_t statements;
+};
 
 Driver::Driver(std::istream &in, std::ostream &out) :
-	in(in), out(out), parser(this),  statements(0)
+	d(new DriverPrivate(in, out))
 {
+}
+
+Driver::~Driver()
+{
+	delete d;
 }
 
 void Driver::run()
 {
-	parser.parse(in);
-}
-
-void Driver::assignment(uint64_t key, uint64_t value)
-{
-	if (key > maxKey || value > maxValue || !stmtCheck()) {
-		error();
-		return;
-	}
-
-	uint32_t nodes = bst.assignment(key, value);
-	out << "wezlow: " << nodes << '\n';
-}
-
-void Driver::sum(uint64_t time, uint64_t left, uint64_t right)
-{
-	if (time > bst.now() || left > right || right > maxKey || !stmtCheck()) {
-		error();
-		return;
-	}
-
-	uint64_t sum = bst.sum(time, left, right);
-	out << "suma(" << time << ',' << left << ".." << right << ")=" << sum << '\n';
-}
-
-void Driver::clear(uint64_t time)
-{
-	if (time > bst.now() || !stmtCheck()) {
-		error();
-		return;
-	}
-
-	uint32_t nodes = bst.clear(time);
-	out << "wezlow: " << nodes << '\n';
-}
-
-void Driver::error()
-{
-	out << "zignorowano\n";
-}
-
-bool Driver::stmtCheck()
-{
-	return statements++ < maxStatements;
+	d->parser.parse(d->in);
 }
--- src/driver.hpp
+++ src/driver.hpp
@@ -1,36 +1,24 @@
 #ifndef DRIVER_HPP
 #define DRIVER_HPP
 
-#include "parser.hpp"
-#include "bst.hpp"
-
 #include <istream>
 #include <ostream>
+
+struct DriverPrivate;
 
 /**
  * Loads, parses and executes commands.
  */
-class Driver : public ParserEventHandler
+class Driver
 {
 public:
 	Driver(std::istream &in, std::ostream &out);
+	~Driver();
 
 	void run();
 
-	void assignment(uint64_t key, uint64_t value) override;
-	void sum(uint64_t time, uint64_t left, uint64_t right) override;
-	void clear(uint64_t time) override;
-	void error() override;
-
 private:
-	bool stmtCheck();
-
-	std::istream &in;
-	std::ostream &out;
-
-	BST bst;
-	Parser parser;
-	uint32_t statements;
+	DriverPrivate *d;
 };
 
 #endif // DRIVER_HPP
